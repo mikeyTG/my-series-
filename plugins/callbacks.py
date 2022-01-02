517e38e5524f04dea4179d871b2d0da79bf8cea6
mikeyTG <88639357+mikeyTG@users.noreply.github.com>
2022-01-02 16:03:27 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -47,12 +47,12 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("look here😶", url="https://telegram.dog/shiro_bot_support"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("Channel", url="https://t.me/series_forums_hub"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/series_forums")
                 ]
             ]
         )
@@ -70,7 +70,7 @@ async def cb_handler(client, query):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "owner😎", url="https://telegram.dog/youtuberyt54355")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
